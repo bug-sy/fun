a3b4365cc13b5a9204b86502a374deb4ef98ce73
Admin <rakesh.sahu694@gmail.com>
2020-01-28 19:48:10 +0530

    modified

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,116 +1,213 @@
-// import * as React from 'react';
-// import { Button } from 'react-native-elements';
-// import { TextInput, Paragraph, Headline } from 'react-native-paper';
-// import { Text, View, Image, ScrollView } from 'react-native';
 
-// export default class Dashboard extends React.Component{
-
-//     render(){
-//         return(
-//             <View>
-// <Paragraph>I am inside the Home Screen</Paragraph>
-//             </View>
-//         )
-//     }
-// }
-
-import { SearchBar } from 'react-native-elements';
 import { Avatar } from 'react-native-elements';
-import { TextInput } from 'react-native-paper';
 import React, { Component } from 'react'
-import { Text, Image, ScrollView  } from 'react-native';
+import { Text, Image, ScrollView } from 'react-native';
 import {
-  StyleSheet,
-  TouchableOpacity,
-  
-  View,
+    StyleSheet,
+    TouchableOpacity,
+
+    View,
 } from 'react-native'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { count: 0,search: '' }
-  }
+    constructor(props) {
+        super(props)
+        this.state = { count: 0, search: '' }
+    }
 
-  onPress = () => {
-    this.setState({
-      count: this.state.count+1
-    })
-  }
 
-  updateSearch = search => {
-    this.setState({ search });
-  };
 
- render() {
-    const { firstQuery } = this.state;
-    const { search } = this.state;
-   return (
-       <ScrollView>
-       <View style={styles.appbar}>
-    <View style={{flexDirection:'row',justifyContent:'center',width:'100%'}}>
-        <View>
-    <TouchableOpacity  style={{width:'100%',height:'100%',backgroundColor:'lime',justifyContent:'center',flexDirection:'row',fontSize:20}}>
-            <Text 
-            style={{fontSize:25}}
-            >
-            Search your Notes
-            </Text>
-    </TouchableOpacity>
-    </View>
-    <View style={{}}>
-    <TouchableOpacity style={{width:'100%',height:'100%',alignItems:'center',backgroundColor:'red'}}>
-      <Image
-         style={{ height:30, width: 40 }}
-        source={require('/home/admin1/Documents/FundooApp/AwesomeProject/image/grid.png')}
-        />
-    </TouchableOpacity>
-    </View> 
+    render() {
+        return (
     
-    <TouchableOpacity
-   
-    >
-       <Avatar rounded title="MD" />
-    </TouchableOpacity>
-    
-   
-    </View>
-     
-      </View>
-      </ScrollView>
-      
-    )
-  }
+                
+            
+                    
+                <View style={{flex:1,flexDirection:'column' }}>
+                        
+                    
+                    <View style={{width:'100%',flexDirection:'row',height:45}}>
+
+                    <View style={{ flexDirection: 'row', width: '80%'}}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <TouchableOpacity style={{ width:'5%' }}>
+                                <Image
+                                    style={{ height: 34, width: 40 }}
+                                    source={require('/home/admin1/Documents/FundooApp/AwesomeProject/image/view_list-512.png')}
+                                />
+                            </TouchableOpacity>
+
+                            
+                            <TouchableOpacity style={{ width: '95%', height: '100%', justifyContent: 'center', flexDirection: 'row' }}>
+                                <Text
+                                    style={{ fontSize: 25 }}
+                                >
+                                    Search your Notes
+                                </Text>
+                            </TouchableOpacity>
+                            
+                        </View>
+                        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', width: '20%' }}>
+                            
+                            <TouchableOpacity >
+                                <Image
+                                    style={{ height: 30, width: 30 }}
+                                    source={require('/home/admin1/Documents/FundooApp/AwesomeProject/image/grid.png')}
+                                />
+                            </TouchableOpacity>
+                            
+                         
+                            
+
+                            <TouchableOpacity
+                                
+                            >
+                                <Avatar rounded title="MD" />
+                            </TouchableOpacity>
+
+                            
+                        </View>
+                    </View>
+                    </View>
+
+               
+               
+
+                
+            
+                
+
+                    <View style={{flex:1,flexDirection:'row',bottom:0,position:'absolute'}}>
+                        <View style={{
+                            flexDirection: 'row',
+                            flex: 1,
+                            
+                            width: '100%',
+                            justifyContent: 'space-between'
+                        }}>
+
+                            <TouchableOpacity style={{ width: '70%' }}>
+                                <Text
+                                    style={{ fontSize: 25, width: '100%' }}
+                                >
+                                    Add a note
+                                </Text>
+
+
+                            </TouchableOpacity>
+
+                                            <View style={styles.bottomicons}>
+
+                                                <TouchableOpacity>
+                                                    <Image
+                                                        style={{ height: 33, width: 40 }}
+                                                        source={require('/home/admin1/Documents/FundooApp/AwesomeProject/image/edit.png')}
+                                                    />
+                                                </TouchableOpacity>
+
+                                                <TouchableOpacity>
+                                                    <Image
+                                                        style={{ height: 32, width: 30 }}
+                                                        source={require('/home/admin1/Documents/FundooApp/AwesomeProject/image/check-box.png')}
+                                                    />
+                                                </TouchableOpacity>
+                                                <TouchableOpacity style={{  height: 34 }}>
+                                                    <Image
+                                                        style={{ height: 32, width: 32 }}
+                                                        source={require('/home/admin1/Documents/FundooApp/AwesomeProject/image/add_image.png')}
+                                                    />
+                                                </TouchableOpacity>
+                                                <TouchableOpacity style={{  height: 34 }}>
+                                                    <Image
+                                                        style={{ height: 32, width: 30 }}
+                                                        source={require('/home/admin1/Documents/FundooApp/AwesomeProject/image/audio_recording.png')}
+                                                    />
+                                                </TouchableOpacity>
+                                            </View>
+
+
+
+                        </View>
+                        </View>
+                        </View>
+                        
+                        
+                        
+                
+
+                        
+
+
+
+
+
+
+
+                
+        
+
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //justifyContent: 'center',
-    paddingHorizontal: 10
-  },
-  button: {
-    //alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    //padding: 10,
-   // alignItems:'center',
-    justifyContent:'flex-start',
-    flexDirection:'column',
-    width:'5%',
-    height:'100%',
-    alignItems:'center'
-  },
-  countContainer: {
-    alignItems: 'center',
-    padding: 10
-  },
-  countText: {
-    color: '#FF00FF'
-  },
-  appbar: {
-      width:'100%',
-      justifyContent:"center",
-      flex:1,
-      backgroundColor:'pink'
-  }
+    container: {
+        flex: 1,
+        //justifyContent: 'center',
+        paddingHorizontal: 10
+    },
+    button: {
+        //alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        //padding: 10,
+        // alignItems:'center',
+        justifyContent: 'flex-start',
+        flexDirection: 'column',
+        width: '5%',
+        height: '100%',
+        alignItems: 'center'
+    },
+    countContainer: {
+        alignItems: 'center',
+        padding: 10
+    },
+    countText: {
+        color: '#FF00FF'
+    },
+    appbar: {
+        width: '100%',
+        justifyContent: "center",
+        flex: 1,
+        alignItems: "stretch",
+        backgroundColor: 'pink'
+    },
+    bottom: {
+
+        flexDirection: 'row',
+        flex: 1,
+        backgroundColor: 'green',
+        // alignItems:'flex-end',
+        width: '90%',
+        justifyContent: 'flex-start'
+
+
+    },
+    bottomicons: {
+        flexDirection: 'row',
+        //backgroundColor: 'blue',
+        width: '30%',
+        justifyContent: 'flex-end'
+    },
+    fullPage:{
+        flex:1,
+        //backgroundColor:'#ff00ff',
+        //height:200
+        flexDirection:'column',
+
+        justifyContent:'space-between'
+        
+    },
+    
+ 
+
 })
